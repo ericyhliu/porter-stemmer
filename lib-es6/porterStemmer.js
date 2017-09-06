7a2b9b0238bc077b7288fca2193e0fa1b136a1ca
Eric Liu <ericliu314@gmail.com>
2017-09-05 20:17:13 -0400

    Refactor methods (step 1 - step 5) in porterStemmer.js

--- lib-es6/porterStemmer.js
+++ lib-es6/porterStemmer.js
@@ -2,23 +2,25 @@
  * 
  * porterStemmer.js
  * 
- * This module is an implementation of Porter's Stemmer, an algorithm used to
- * remove the commoner morphological and inflexional endings from words in 
- * English, as a method of normalization.
+ * This module is an implementation of Porter's Stemmer Algorithm, an algorithm used to
+ * remove the commoner morphological and inflexional endings from words in English, as 
+ * a method of normalization in natural language processing.
+ * 
+ * Author: Eric Liu (https://github.com/eliucs)
  * 
  */
 
 class PorterStemmer {
 
     constructor() {
-        const c = '[^aeiou]';
-        const v = '[aeiouy]';
-        const C = `${c}[^aeiouy]*`;
-        const V = `${v}[aeiou]*`;
-        this.mgr0 = `^(${C})?${V}${C}`;
-        this.meq1 = `^(${C})?${V}${C}(${V})?$`;
-        this.mgr1 = `^(${C})?${V}${C}${V}${C}`;
-        this.hasVowel = `^(${C})?${v}`;
+        this.c = '[^aeiou]';
+        this.v = '[aeiouy]';
+        this.C = `${this.c}[^aeiouy]*`;
+        this.V = `${this.v}[aeiou]*`;
+        this.mgr0 = `^(${this.C})?${this.V}${this.C}`;
+        this.meq1 = `^(${this.C})?${this.V}${this.C}(${this.V})?$`;
+        this.mgr1 = `^(${this.C})?${this.V}${this.C}${this.V}${this.C}`;
+        this.hv = `^(${this.C})?${this.v}`;
 
         this.step1a = {
             'sses': 'ss',
@@ -99,173 +101,182 @@ class PorterStemmer {
         };
     }
 
-    stem(w) {
-        let original = w;
+    normalize(w) {
+        return w.toLowerCase().replace(/[^a-zA-Z]+/g, '');
+    }
 
-        // Normalize w:
-        w = w.toLowerCase().replace(/[^a-zA-Z]+/g, '');
-        console.log(w);
+    hasDuplicatedLastLetter(w) {
+        return w[w.length - 1] && w[w.length - 2] &&  w[w.length - 1] === w[w.length - 2];
+    }
 
-        // Step 1a:
-        console.log('\n--Step 1--')
+    hasEndingCVC(w) {
+        return w[w.length - 1] && w[w.length - 2] && 
+        w[w.length - 3] && 
+        /[^aeiou]/.test(w[w.length - 3]) && 
+        /[^aeiou]/.test(w[w.length - 1]) && 
+        /[aeiouy]/.test(w[w.length - 2]);
+    }
+
+    doStep1a(w) {
         let step1aMatch = Object.keys(this.step1a)
         .filter((s) => {
             return new RegExp(`${s}$`).test(w)
         })
-        .map((s) => {
-            return {
-                stem: s,
-                len: s.length
-            };
-        })
-        .reduce((prev, current) => {
-            return (prev.len > current.len) ? prev : current;
+        .reduce((previous, current) => {
+            return (previous.length > current.length) ? previous : current;
         }, false);
-        console.log(step1aMatch);
 
         if (step1aMatch) {
-            w = w.replace(new RegExp(`${step1aMatch.stem}$`), 
-                this.step1a[step1aMatch.stem]);
+            w = w.replace(new RegExp(`${step1aMatch}$`), 
+                this.step1a[step1aMatch]);
         }
-        console.log(w);
+        return w;
+    }
 
-        // Step 1b:
-        // (m > 0) EED -> EE
-        let secondOrThird = false;
+    doStep1b(w) {
+        // Part 1:
+        let doPart2 = false;
         if (w.endsWith('eed') && (new RegExp(this.mgr0).test(w.substr(0, 
             w.lastIndexOf('eed'))))) {
             w = w.replace(new RegExp('eed$'), this.step1b['eed']);
-        } else if (w.endsWith('ed') && (new RegExp(this.hasVowel).test(w.substr(0, 
+        } else if (w.endsWith('ed') && (new RegExp(this.hv).test(w.substr(0, 
             w.lastIndexOf('ed'))))) {
             w = w.replace(new RegExp('ed$'), this.step1b['ed']);
-            secondOrThird = true;
-        } else if (w.endsWith('ing') && (new RegExp(this.hasVowel).test(w.substr(0, 
+            doPart2 = true;
+        } else if (w.endsWith('ing') && (new RegExp(this.hv).test(w.substr(0, 
             w.lastIndexOf('ing'))))) {
             w = w.replace(new RegExp('ing$'), this.step1b['ing']);
-            secondOrThird = true;
+            doPart2 = true;
         }
 
-        // Only triggered when the second or third rule of Step 1b is applied:
-        if (w.endsWith('at')) {
-            w = w.replace(new RegExp('at$'), this.step1b['at']);
-        } else if (w.endsWith('bl')) {
-            w = w.replace(new RegExp('bl$'), this.step1b['bl']);
-        } else if (w.endsWith('iz')) {
-            w = w.replace(new RegExp('iz$'), this.step1b['ize']);
-        } else if (w[w.length - 1] && w[w.length - 2] && 
-            w[w.length - 1] === w[w.length - 2] && 
-            !(w[w.length - 1] === 'l' || w[w.length - 1] === 's' || w[w.length - 1] === 'z')) {
-            w = w.replace(new RegExp(`${w[w.length - 1]}${w[w.length - 1]}$`), w[w.length - 1]);
-        } else if ((new RegExp(this.meq1).test(w)) && w[w.length - 1] && w[w.length - 2] && 
-            w[w.length - 3] && /[^aeiou]/.test(w[w.length - 3]) && /[^aeiou]/.test(w[w.length - 1])
-            && /[aeiouy]/.test(w[w.length - 2])) {
-            w += 'e';
+        // Part 2 - only triggered when the second or third rule of the 
+        // above is applied:
+        if (doPart2) {
+            if (w.endsWith('at')) {
+                w = w.replace(new RegExp('at$'), this.step1b['at']);
+            } else if (w.endsWith('bl')) {
+                w = w.replace(new RegExp('bl$'), this.step1b['bl']);
+            } else if (w.endsWith('iz')) {
+                w = w.replace(new RegExp('iz$'), this.step1b['iz']);
+            } else if (this.hasDuplicatedLastLetter(w) && 
+                !(w.endsWith('l') || 
+                  w.endsWith('s') ||
+                  w.endsWith('z'))) {
+                const c = w[w.length - 1];
+                w = w.replace(new RegExp(`${c}${c}$`), c);
+            } else if (this.hasEndingCVC(w) && (new RegExp(this.meq1).test(w))) {
+                w += 'e';
+            }
         }
+        return w;
+    }
 
-        // Step 1c:
-        if (w.endsWith('y') && (new RegExp(this.hasVowel).test(w.substr(0, 
+    doStep1c(w) {
+        if (w.endsWith('y') && (new RegExp(this.hv).test(w.substr(0, 
             w.lastIndexOf('y'))))) {
-            w = w.replace(new RegExp('y$'), this.step1c['i']);
+            w = w.replace(new RegExp('y$'), this.step1c['y']);
         }
+        return w;
+    }
 
-        // Step 2:
-        console.log('\n--Step 2--');
+    doStep2(w) {
         let step2Match = Object.keys(this.step2)
         .filter((s) => {
             const last = w.lastIndexOf(s);
-            return (last === -1) ? false : new RegExp(this.mgr0).test(w.substr(0, last));
+            return (last === -1) ? false : 
+                new RegExp(this.mgr0).test(w.substr(0, last));
         })
-        .map((s) => {
-            return {
-                stem: s,
-                len: s.length
-            };
-        })
-        .reduce((prev, current) => {
-            return (prev.len > current.len) ? prev : current;
+        .reduce((previous, current) => {
+            return (previous.length > current.length) ? previous : current;
         }, false);
-        console.log(step2Match);
 
         if (step2Match) {
-            w = w.replace(new RegExp(`${step2Match.stem}$`), 
-                this.step2[step2Match.stem]);
+            w = w.replace(new RegExp(`${step2Match}$`), 
+                this.step2[step2Match]);
         }
-        console.log(w);
+        return w;
+    }
 
-        // Step 3:
-        console.log('\n--Step 3--');
+    doStep3(w) {
         let step3Match = Object.keys(this.step3)
         .filter((s) => {
             const last = w.lastIndexOf(s);
-            return (last === -1) ? false : new RegExp(this.mgr0).test(w.substr(0, last));
+            return (last === -1) ? false : 
+                new RegExp(this.mgr0).test(w.substr(0, last));
         })
-        .map((s) => {
-            return {
-                stem: s,
-                len: s.length
-            };
-        })
-        .reduce((prev, current) => {
-            return (prev.len > current.len) ? prev : current;
+        .reduce((previous, current) => {
+            return (previous.length > current.length) ? previous : current;
         }, false);
-        console.log(step3Match);
 
         if (step3Match) {
-            w = w.replace(new RegExp(`${step3Match.stem}$`), 
-                this.step3[step3Match.stem]);
+            w = w.replace(new RegExp(`${step3Match}$`), 
+                this.step3[step3Match]);
         }
-        console.log(w);
+        return w;
+    }
 
-        // Step 4:
-        console.log('\n--Step 4--');
+    doStep4(w) {
         let step4Match = Object.keys(this.step4)
         .filter((s) => {
             const last = w.lastIndexOf(s);
-            // Handle case of (m > 1 && (*S or *T)) ION -> <null>
+            // Handle case of (m > 1 && (*s or *t)) 'ION' -> <null>
             if (s === 'ion') {
                 const pre = w.substr(0, last);
                 return (last === -1) ? false : new RegExp(this.mgr1).test(pre)
                     && (pre.endsWith('s') || pre.endsWith('t'));
             }
-            return (last === -1) ? false : new RegExp(this.mgr1).test(w.substr(0, last));
+            return (last === -1) ? false : 
+                new RegExp(this.mgr1).test(w.substr(0, last));
         })
-        .map((s) => {
-            return {
-                stem: s,
-                len: s.length
-            };
-        })
-        .reduce((prev, current) => {
-            return (prev.len > current.len) ? prev : current;
+        .reduce((previous, current) => {
+            return (previous.length > current.length) ? previous : current;
         }, false);
-        console.log(step4Match);
 
         if (step4Match) {
-            w = w.replace(new RegExp(`${step4Match.stem}$`), 
-                this.step4[step4Match.stem]);
+            w = w.replace(new RegExp(`${step4Match}$`), 
+                this.step4[step4Match]);
         }
-        console.log(w);
-
-        // Step 5a:
-        if (w.endsWith('e') && (new RegExp(this.mgr1).test(w.substr(0, w.lastIndexOf('e'))))) {
-            w = w.slice(0, w.length - 1);
-        } else if (w.endsWith('e') && !(w[w.length - 2] && w[w.length - 3] && 
-            w[w.length - 4] && /[^aeiou]/.test(w[w.length - 4]) && /[^aeiou]/.test(w[w.length - 2])
-            && /[aeiouy]/.test(w[w.length - 3])) && (new RegExp(this.meq1).test(w.substr(0, w.lastIndexOf('e'))))) {
-            w = w.slice(0, w.length - 1);
-        }
-
-        // Step 5b:
-        if (w.endsWith('l') && w[w.length - 1] && w[w.length - 2] && w[w.length - 1] === w[w.length - 2] && 
-            (new RegExp(this.mgr1).test(w.substr(0, w.length - 1)))) {
-            w = w.replace(new RegExp(`${w[w.length - 1]}${w[w.length - 1]}$`), w[w.length - 1]);
-        }
-        console.log(w);
-
         return w;
     }
 
+    doStep5a(w) {
+        if (w.endsWith('e') && (new RegExp(this.mgr1).test(w.substr(0, 
+            w.lastIndexOf('e'))))) {
+            w = w.slice(0, -1);
+        } else if (w.endsWith('e') && !(this.hasEndingCVC(w.slice(0, -1))) && 
+            (new RegExp(this.meq1).test(w.substr(0, w.lastIndexOf('e'))))) {
+            w = w.slice(0, -1);
+        }
+        return w;
+    }
+
+    doStep5b(w) {
+        if (w.endsWith('l') && this.hasDuplicatedLastLetter(w) && 
+            (new RegExp(this.mgr1).test(w.substr(0, w.length - 1)))) {
+            const c = w[w.length - 1];
+            w = w.replace(new RegExp(`${c}${c}$`), c);
+        }
+        return w;
+    }
+
+    stem(w) {
+        w = this.normalize(w);
+        w = this.doStep1a(w);
+        w = this.doStep1b(w);
+        w = this.doStep1c(w);
+        w = this.doStep2(w);
+        w = this.doStep3(w);
+        w = this.doStep4(w);
+        w = this.doStep5a(w);
+        w = this.doStep5b(w);
+
+        console.log(w);
+        return w;
+    }
 }
+
+let p = new PorterStemmer();
+p.stem('roll');
 
 module.exports = {
     PorterStemmer
